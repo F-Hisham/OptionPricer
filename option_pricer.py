bd47f2db7f83bc3b655a7b2601bfd53c590fe193
F-Hisham <hisham.mirza@gmail.com>
2022-10-11 08:45:38 +0100

    Initial commit

---
+++ option_pricer.py
@@ -0,0 +1,34 @@
+import scipy.stats
+import numpy as np
+
+
+class OptionPricer:
+    def __init__(self, s, k, r, t, sigma):
+        self.spot = s
+        self.strike = k
+        self.rate = r
+        self.time = t
+        self.sigma = sigma
+
+    def discounting(self) -> float:
+        return np.exp(-self.rate * self.time)
+
+    def black_scholes(self, c_p: True):
+        n = scipy.stats.norm.cdf
+        d1 = (np.log(self.spot / self.strike) + (self.rate + self.sigma ** 2 / 2) * self.time) / (
+                self.sigma * np.sqrt(self.time))
+        d2 = d1 - self.sigma * np.sqrt(self.time)
+        # c = n(d1) * self.spot - n(d2) * self.strike * np.exp(-self.rate * self.time)
+        # p = c + self.strike * np.exp(-self.rate * self.time) - self.spot
+        if c_p:
+            return n(d1) * self.spot - n(d2) * self.strike * np.exp(-self.rate * self.time)
+        else:
+            return n(-d2) * self.strike * np.exp(-self.rate * self.time) - n(-d1) * self.spot
+
+    def monte_carlo(self, steps, paths):
+        dt = self.time / steps
+        path = np.exp(np.log(self.spot) +
+                      np.cumsum(((self.rate - self.sigma ** 2 / 2) * dt +
+                                 self.sigma * np.sqrt(dt) * np.random.normal(size=(steps, paths))), axis=0))
+        return np.mean(np.maximum(path[-1] - self.strike, 0)) * self.discounting(), np.mean(
+            np.maximum(self.strike - path[-1], 0)) * self.discounting()
